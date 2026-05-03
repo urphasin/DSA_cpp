6be6b77daf67801dd38f63079a58207bc6591703
Otito Amuga <urphasin@gmail.com>
2026-05-03 13:54:55 -0600

    foolish

--- cpp_tutorial/md.cpp
+++ cpp_tutorial/md.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  int mat[3][5] = {1,1,1,1,1,2,2,2,2,2,3,3,3,3,3}; // m x n = 3 x 5;
+  int mat[3][5] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // m x n = 3 x 5;
   for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 3; j++) {
       cout << mat[j][i] << " ";
