f9dd5f44c256253b7800f2e6e600df256d9e8d71
Otito Amuga <urphasin@gmail.com>
2026-05-03 11:49:59 -0600

    sds

--- cpp_tutorial/md.cpp
+++ cpp_tutorial/md.cpp
@@ -12,9 +12,9 @@ int main() {
   cout << "\n\n";
 
   int mat[3][5] = {1,1,1,1,1,2,2,2,2,2,3,3,3,3,3}; // m x n = 3 x 5;
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 3; j++) {
-      cout << mat[j][i] << " ";
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      cout << mat[i][j] << " ";
     }
     cout << endl;
   }
